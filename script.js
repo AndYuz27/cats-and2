518df97cdcede7db891fc32dfab60d9d69c898ff
AndYuz27 <yuzlov2003@gmail.com>
2022-09-14 18:48:29 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+/*{
+"id": 1,
+"name": "Лара",
+"favourite": false,
+"rate": 7,
+"age": 8,
+"description": "описание",
+"img_link": "ссылка на картинку в интернете"
+} */
+const box = document.querySelector(".container");
+
+const getCats = function(){ //IIFE
+    fetch(" http://sb-cats.herokuapp.com/api/2/andy1337/show")
+    .then(res => res.json())
+    .then(data => {
+        data.data.forEach(el => {
+            if(el.id && el.name) {
+
+let card = `
+    <div class="card" ">
+    <div class="card-img" style="${el.img_link && `background-image: url(${el.img_link})`}"></div>
+        <h4>${el.name}</h4>
+        <h5></h5>
+    </div>`;
+box.innerHTML += card;
+
+
+            }
+        });
+    })
+    
+};
+getCats();
+
+const form = document.forms.addCat;
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const body = {}
+    for (let i = 0; i < form.elements.length; i++) {
+        const el = form.elements[i];
+        if (el.name && el.value) {
+            body[el.name] =el.value
+        }
+        console.log(body)
+    }
+    fetch("http://sb-cats.herokuapp.com/api/2/andy1337/add",{
+        method: "POST",
+        headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(body)
+
+}
+    ).then(res => res.json())
+    .then(data => {
+        console.log(data)
+        if(data.message === "ok"){
+            box.innerHTML = ""
+            getCats();
+        }
+    })
+})
+
+
+
+
+function info_create(){
+    alert("Чтобы добавить, введите имя*, далее id*, рейтинг, возраст, описание и картинку, учтите, картинку нужно брать с интернета (URL-ссылкой), если хотите свою, то заливайте на Яндекс диск либо на другой фотохостинг")
+    console.log('Вызвана инфа')
+}
